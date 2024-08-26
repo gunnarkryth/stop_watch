1b30ca1a0e4426f09be1075af6bc101b4cdbf1d9
Gunski <44674438+gunnarkryth@users.noreply.github.com>
2024-08-26 13:57:41 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./assets/styles/App.scss";
+import { Wrapper } from "./components/Wrapper/Wrapper";
+import { Atom } from "./components/Atom/Atom";
+
+function App() {
+  return (
+    <Wrapper>
+      <Atom></Atom>
+    </Wrapper>
+  );
+}
+
+export default App;
